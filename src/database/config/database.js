7c55741f8bd8d965bed3ba90b93e71be71e5c29e
Kirill Mukomela <61352634+koros-rk@users.noreply.github.com>
2023-12-11 20:23:05 +0200

    db config fix

--- src/database/config/database.js
+++ src/database/config/database.js
@@ -5,21 +5,21 @@ module.exports = {
     "username": process.env.DB_USERNAME,
     "password": process.env.DB_PASSWORD,
     "database": process.env.DB_DEV_NAME,
-    "host": "127.0.0.1",
+    "host": process.env.DB_HOST,
     "dialect": "postgres"
   },
   "test": {
     "username": process.env.DB_USERNAME,
     "password": process.env.DB_PASSWORD,
     "database": process.env.DB_NAME,
-    "host": "127.0.0.1",
+    "host": process.env.DB_HOST,
     "dialect": "postgres"
   },
   "production": {
     "username": process.env.DB_USERNAME,
     "password": process.env.DB_PASSWORD,
     "database": process.env.DB_PROD_NAME,
-    "host": "127.0.0.1",
+    "host": process.env.DB_HOST,
     "dialect": "postgres"
   }
 }
