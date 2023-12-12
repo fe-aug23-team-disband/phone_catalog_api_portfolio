fd4f634dc7f097e2d8b4c8b7bcde6d37603e2f9e
Kirill Mukomela <61352634+koros-rk@users.noreply.github.com>
2023-12-12 17:40:48 +0200

    db config fix

--- deploy.sh
+++ deploy.sh
@@ -2,12 +2,12 @@
 
 npm i
 
+npx sequelize-cli db:migrate:undo:all
 npx sequelize-cli db:migrate
 
 echo "Migration complete"
 
 npx sequelize-cli db:seed:undo:all
-
 npx sequelize-cli db:seed:all
 
 echo "Seeding complete"
